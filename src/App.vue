ef0de3e780f142618e40207d817379776aeeaa89
Miguel-Angel-Laurero <malaurero@alu.fpsanantonio.com>
2025-11-26 17:16:06 +0100

    pini end

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,18 @@
 </template>
 
 <style>
+*{
+  margin:0;
+  padding: 0;
+}
+#app{
+    background-color: #597d9a;
+    display:flex;
+    justify-content: center;
+    font-family: Avenir,Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color:#2c3e50;
+}
 </style>
--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -31,4 +31,31 @@ function addItemAndClear(item){
     text-align: center;
     color:#2c3e50;
 }
+form {
+  margin-bottom: 15px;
+}
+input {
+  margin-top: 25px;
+  margin-bottom: 15px;
+  height: 20px;
+  width: 50%;
+}
+button {
+  margin-left: 30px;
+  background-color: #2f6089;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  font-weight: 800;
+  color: white;
+  width: 15%;
+}
+.alert-div {
+  min-height: 25px;
+}
+.alert {
+  color: #d1495b;
+  font-size: 1em;
+  font-weight: 600;
+}
 </style>
